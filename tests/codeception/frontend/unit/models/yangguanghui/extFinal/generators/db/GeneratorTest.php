84631f28d781b7f5c204b4e51eb956784a1ef3cd
yangguanghui <252699464@qq.com>
2016-08-05 21:42:00 +0800

    modify final db unit test

--- tests/codeception/frontend/unit/models/yangguanghui/extFinal/generators/db/GeneratorTest.php
+++ tests/codeception/frontend/unit/models/yangguanghui/extFinal/generators/db/GeneratorTest.php
@@ -14,7 +14,7 @@ class GeneratorTest extends TestCase
 
     private $source = YII_APP_BASE_PATH . '/common/config/main-local.php';
     private $dest = YII_APP_BASE_PATH 
-            . '/tests/codeception/frontend/unit/fixtures/data' 
+            . '/tests/codeception/frontend/unit/fixtures/data/models/' 
             . 'yangguanghui/extFinal/generators/db/main-local.php';
     
     protected function setUp()
@@ -40,12 +40,12 @@ class GeneratorTest extends TestCase
             'dbName' => 'test-final',
         ];
 
-        expect('DB test-final should be create', $model->save())->true();
+        expect('DB test-final should be create', $model->save(null, null, $result))->true();
         
         $config = require($model->configFile);
         
         $this->specify('config file should content model data', function () use ($model, $config) {
-            expect('db name should be test-final', $config['components']['db'])->equals($model->dbName);
+            expect('db name should be test-final', $config['components']['db']['dsn'])->contains($model->dbName);
         });
     }
 
